172077537bc0ecef280b9e90fdc1fee77a7517aa
Ranjithkumar444 <ranjithrockzz289@gmail.com>
2025-02-01 15:38:23 +0530

    tables name change

--- src/main/java/com/springsecuritydemo/securitydemo/Model/Users.java
+++ src/main/java/com/springsecuritydemo/securitydemo/Model/Users.java
@@ -1,40 +1,19 @@
 package com.springsecuritydemo.securitydemo.Model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Entity
-@Table(name = "Users")
+@Table(name = "users")
 public class Users {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String username;
     private String password;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 
     @Override
     public String toString() {
